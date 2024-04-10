d52447c9a19976c3df81f8afaa2b6e0548ef9256
Michael Fessenden <michael.fessenden@gmail.com>
2024-04-10 12:11:01 -0400

    feat: add 'filter' input to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,35 @@
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
-function SDLNavbar() {
+export default function SDLNavbar() {
     return (
-        <>
-            <Navbar className='bg-body-tertiary'>
-                <Container>
-                    <Navbar.Brand href='#home'>
-                        <img
-                            alt=''
-                            src='images/sdl-header.svg'
-                            width='278'
-                            height='48'
-                            className='d-inline-block align-top'
-                        />{' '}
-                    </Navbar.Brand>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar className='bg-body-tertiary justify-content-between'>
+            <Container fluid>
+                <Navbar.Brand href='#home'>
+                    <img
+                        alt=''
+                        src='images/sdl-header.svg'
+                        width='278'
+                        height='48'
+                        className='d-inline-block align-top'
+                    />{' '}
+                </Navbar.Brand>
+            </Container>
+            <Form inline className='justify-content-end'>
+                <Row>
+                    <Col xs='auto'>
+                        <Form.Control
+                            type='text'
+                            placeholder='Filter'
+                            className=' mr-sm-2'
+                        />
+                    </Col>
+                </Row>
+            </Form>
+        </Navbar>
     );
 }
-
-
-export default SDLNavbar;
