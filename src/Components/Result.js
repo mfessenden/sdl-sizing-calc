a7fb1b7e8ec8cd24fbf85248f690cdeebd53c0b7
Michael Fessenden <michael.fessenden@gmail.com>
2024-04-11 16:45:13 -0400

    chore: remove unused module, cleanup dropdown logic

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -60,8 +60,9 @@ function TabbedResultOutput() {
                         size='sm'
                         value={currentState.retention_period_value}
                         className='text-center'
-                        onChange={setRetentionValue}
+                        onChange={e => setRetentionValue(e.target.value)}
                     />
+
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>No. of Employees</Form.Label>
@@ -69,16 +70,16 @@ function TabbedResultOutput() {
                         size='sm'
                         value={currentState.employee_count}
                         className='text-center'
-                        onChange={setEmployeeCount}
+                        onChange={e => setEmployeeCount(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>SOAR Seats</Form.Label>
                     <Form.Control
                         size='sm'
-                        value='0'
+                        value={currentState.soar_seats}
                         className='text-center'
-                        onChange={setSeatCount}
+                        onChange={e => setSeatCount(e.target.value)}
                     />
                 </Form.Group>
             </Form>
@@ -131,169 +132,8 @@ function ResultTabs() {
 }
 
 
-const DropDownData = {
-    industry: [
-        {
-            id: 0,
-            name: 'energy',
-            display_name: 'Energy / Utilities',
-            weight: 0.95
-        },
-        {
-            id: 1,
-            name: 'consumer_product',
-            display_name: 'Consumer - Products/Equipment',
-            weight: 1
-        },
-        {
-            id: 2,
-            name: 'consumer_retail',
-            display_name: 'Consumer - Retail/Services',
-            weight: 2
-        },
-        {
-            id: 3,
-            name: 'financial_insurance',
-            display_name: 'Financial - Insurance / Services',
-            weight: 1.2
-        },
-        {
-            name: 'financial_retail',
-            display_name: 'Financial - Retail',
-            weight: 2
-        },
-        {
-            id: 4,
-            name: 'government_federal',
-            display_name: 'Government - Federal',
-            weight: 1.4
-        },
-        {
-            id: 5,
-            name: 'government_local',
-            display_name: 'Government - Local',
-            weight: 0.75
-        },
-        {
-            id: 6,
-            name: 'government_state',
-            display_name: 'Government - State',
-            weight: 1
-        },
-        {
-            id: 7,
-            name: 'healthcare_services',
-            display_name: 'Health Care Services',
-            weight: 1.2
-        },
-        {
-            id: 8,
-            name: 'higher_education',
-            display_name: 'Higher Education',
-            weight: 2.5
-        }
-    ],
-    industry_size: [
-        {
-            name: 'regional',
-            display_name: 'Regional / SMB',
-            weight: 0.25
-        },
-        {
-            name: 'small',
-            display_name: 'Small / Fortune 2000',
-            weight: 0.75
-        },
-        {
-            name: 'medium',
-            display_name: 'Medium / Fortune 1000',
-            weight: 1
-        },
-        {
-            name: 'large',
-            display_name: 'Large / Fortune 500',
-            weight: 1.75
-        },
-        {
-            name: 'global',
-            display_name: 'Global / Fortune 200',
-            weight: 3
-        }
-    ],
-    org_size: [
-        {
-            name: 'size1',
-            display_name: 'Under 500',
-            weight: 0.25
-        },
-        {
-            name: 'size2',
-            display_name: '500 to 2000',
-            weight: 0.65
-        },
-        {
-            name: 'size3',
-            display_name: '2000 to 5000',
-            weight: 1
-        },
-        {
-            name: 'size4',
-            display_name: '5000 to 10000',
-            weight: 1.45
-        },
-        {
-            name: 'size5',
-            display_name: '10000 to 25000',
-            weight: 2
-        },
-        {
-            name: 'size6',
-            display_name: '25000 to 40000',
-            weight: 2.75
-        },
-        {
-            name: 'size7',
-            display_name: '40000 to 65000',
-            weight: 4.25
-        },
-        {
-            name: 'size8',
-            display_name: '65000 to 85000',
-            weight: 5
-        },
-        {
-            name: 'size9',
-            display_name: 'Over 85000',
-            weight: 5.85
-        }
-    ]
-}
-
-
-
-function DropDownItem({name, display_name, menuItems}) {
-    return (
-        <DropdownButton
-            id={name}
-            title={display_name}
-            variant='secondary'
-        >
-            {/*<Dropdown.ItemText>{item.description}</Dropdown.ItemText>*/}
-            {menuItems.map(menuItem => (
-                <Dropdown.Item
-                    key={menuItem.id}
-                    as='button'>
-                    {menuItem.display_name}
-                </Dropdown.Item>
-            ))}
-        </DropdownButton>
-    );
-}
-
 
 export default function ResultComponent({useButton = true}) {
-    const data = useStateStore()
-    const dropdown_data = DropDownData.industry
 
     return (
         <Card>
@@ -302,23 +142,12 @@ export default function ResultComponent({useButton = true}) {
                 Your estimated data ingest:
             </Row>
             <Row className='align-center result-xl'>
-                {humanFileSize(data.usage)}
+                {humanFileSize(0)}
             </Row>
             <Row className='align-center'>
                 <ResultTabs/>
             </Row>
 
-            <Row>
-                <Container>
-                    <Row>
-                        <DropDownItem
-                            name='industry'
-                            display_name='Industry'
-                            menuItems={dropdown_data}
-                        />
-                    </Row>
-                </Container>
-            </Row>
 
             {useButton &&
                 <Row>
