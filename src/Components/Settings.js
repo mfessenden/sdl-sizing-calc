2dfb33ae9400cc608dd8f4b8ab44516762582181
Michael Fessenden <michael.fessenden@gmail.com>
2024-04-23 12:12:53 -0400

    feat: update settings panel

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -4,48 +4,79 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import {useStateStore} from '../Model/Data';
+import {SETTINGS_PANEL_TITLE} from '../Constants';
+import React from 'react';
 
 
-export default function SettingsBody() {
-    const {state, actions: {toggleDebug}} = useStateStore();
+/**
+ * Renders a settings panel with options for administering the app.
+ *
+ * @return {JSX.Element} rendered settings panel.
+ */
+export default function SettingsPanel() {
+    const {state} = useStateStore();
+    const dataSources = state.current_state.data_sources
+    console.log('Data Sources: ')
+    console.log(dataSources)
+
+    const handleChange = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        // setValidated(true);
+    };
+
     return (
-        <Container fluid className='d-grid gap-3'>
-            <Row>
-                <Col md={8}>
-                    <Card className='m-2 p-3'>
-                        <Card.Title>Settings</Card.Title>
-                        <Card.Body className='m-1'>
-                            <Form>
-                                <Form.Check // prettier-ignore
-                                    type='switch'
-                                    id='switch-debug'
-                                    label='Admin Mode'
-                                    value={state.current_state.debug_mode}
-                                    onChange={e => toggleDebug(e.target.value)}
-                                />
-                            </Form>
-                        </Card.Body>
-                        <Card.Title>Data Sources</Card.Title>
-                        <Card.Body className='m-1'>
-                            <Form>
-                                <Form.Control
-                                    as='select'
-                                    custom
-                                >
-                                    <option value='black'>Black</option>
-                                    <option value='amber'>Amber</option>
-                                    <option value='purple'>Purple</option>
-                                    <option value='magenta'>Magenta</option>
-                                    <option value='white'>White</option>
-                                </Form.Control>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4}>
+        <Form>
+            <Container fluid className='d-grid gap-3'>
+                <Row>
+                    <Col md={8}>
+                        <Card className='m-0'>
+                            <Card.Header className='admin-panel-header'>
+                                {SETTINGS_PANEL_TITLE}
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Title>Data Sources</Card.Title>
+                                <Row className='p-3'>
+                                    <Form.Control as='select' className='input-mono'>
+                                        {dataSources.map((dataSource) => (
+                                            <>
+                                                <option key={dataSource.id}>
+                                                    {dataSource.path}
+                                                </option>
+                                            </>
+                                        ))}
+                                    </Form.Control>
+                                </Row>
+                                {/*<Card.Title>Add Source</Card.Title>*/}
+                                <Row className='p-3'>
+                                    <Form.Group className='position-relative mb-3'>
+                                        <Form.Label className='data-edit-label'>Add Source</Form.Label>
+                                        <Form.Control
+                                            type='file'
+                                            required
+                                            name='file'
+                                            onChange={handleChange}
+                                            isInvalid={false}
+                                        />
+                                        <Form.Control.Feedback type='invalid' tooltip>
 
-                </Col>
-            </Row>
-        </Container>
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+
+                    </Col>
+                </Row>
+            </Container>
+        </Form>
     )
 }
